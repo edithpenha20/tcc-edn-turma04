fa7197e81aed5b9db8fbd12728bc77591a1ba571
edpenha <edithpenha@gmail.com>
2023-06-06 11:06:26 -0300

    tcc turma 04

---
+++ script.js
@@ -0,0 +1,16 @@
+// menu
+let list = document.querySelectorAll('.nav li');
+function active(){
+    list.forEach((i) =>
+    i.classList.remove('active'));
+    this.classList.add('active');
+}
+
+list.forEach((i) =>
+i.addEventListener('click', active));
+
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+menuToggle.onclick = function(){
+    header.classList.toggle('active');
+}
